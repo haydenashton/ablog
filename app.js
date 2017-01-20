ccdd79cdad5ab403ab4fbd50ad14a20dc10f2d2d
Hayden Ashton <hayden.ashton@vocus.co.nz>
2017-01-20 20:33:12 +1300

    Add Github auth to /api

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ let fs = require('fs');
 let path = require('path');
 let express = require('express');
 let app = express();
-let bodyParser = require('body-parser');
 let config = require('./config');
 let apiPath = path.join(__dirname, 'api');
 
@@ -17,24 +16,30 @@ app.set('mongouri', (process.env.MONGODB_URI || config.mongouri));
 let mongoose = require('./config/mongoose')(app);
 let passport = require('./config/auth')(mongoose.models.User);
 
-app.use(express.static(__dirname + '/public'));
+app.use(require('cookie-parser')());
+app.use(require('body-parser').json());
+app.use(require('express-session')({
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true
+}));
+
 app.use(passport.initialize());
 app.use(passport.session());
 
 app.get('/auth/github', passport.authenticate('github'));
-
 app.get('/auth/github/callback',
-  passport.authenticate('github', { failureRedirect: '/auth/github' }),
+  passport.authenticate('github', { successReturnToOrRedirect: '/', failureRedirect: '/auth/github' }),
   function (req, res) {
-    // Successful authentication, redirect home.
     res.redirect('/');
   });
 
-app.use(bodyParser.json());
-
+app.all('/api/*', require('connect-ensure-login').ensureLoggedIn('/auth/github'));
 appVersions.forEach((version) => {
   app.use(`/api/${version}`, require(path.join(__dirname, 'api', version)).routes);
 });
+
+app.use(express.static(__dirname + '/public'));
 
 app.listen(app.get('port'), () => {
   console.log('Node app is running on port', app.get('port'));
